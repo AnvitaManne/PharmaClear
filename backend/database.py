ee995d0e0505087d31412c0bd5ce276c316e843c
AnvitaManne <anvita.manne@gmail.com>
2025-11-06 22:15:13 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from .config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+from sqlalchemy import create_engine
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
